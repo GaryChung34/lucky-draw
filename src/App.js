f8d7efcf2222b3717eba9255c8951dbf10a00311
gary-home <gary64800@gmail.com>
2021-10-18 02:14:10 +0800

    add build pack.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className='container mx-auto py-10'>
       <button className='bg-green-400 rounded-lg shadow-lg text-white text-xl p-3 font-bold'>click me</button>
-      <div className='text-black text-3xl font-bold py-5'>Hello World</div>
+      <div className='text-black text-3xl font-bold py-5'>Hello World added build pack.</div>
     </div>
   );
 }
